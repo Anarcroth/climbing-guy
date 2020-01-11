face72372f1b3ad13d0d0f1b7d8705cf4273bd5f
Martin Nestorov <mnestorov@protonmail.com>
2020-01-11 22:24:58 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject climbing-guy "0.1.0-SNAPSHOT"
+  :description "climbing-guy: this guy climbs your screen"
+  :url "https://github.com/Anarcroth/climbing-guy"
+  :license {:name "GNU General Public License"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [quil "3.1.0"]]
+  :main climbing-guy.core)
